efac7abc41a5d5f515607d5c51c985a5b045a214
Filipe Góis <filipeogois@gmail.com>
2026-02-01 21:27:41 -0300

    .

--- matrizes/vendas.py
+++ matrizes/vendas.py
@@ -1,16 +1,16 @@
 vendas = [
-    [120, 90, 150, 80],    
-    [60, 200, 100, 140],  
-    [130, 110, 90, 160]   
+    [120, 90, 150, 80],
+    [60, 200, 100, 140],
+    [130, 110, 90, 160]
 ]
 
-total_vendas_vendedores : list[int] = []
-total_vendas_por_dia : list[int] = []
+total_vendas_vendedores: list[int] = []
+total_vendas_por_dia: list[int] = []
 maior_vendedor = {}
 maior_individual = {
-         "iDia":0,
-                "iVendedor":0,
-                "total": 0
+    "iDia": 0,
+    "iVendedor": 0,
+    "total": 0
 }
 
 for i in range(len(vendas)):
@@ -19,32 +19,31 @@ for i in range(len(vendas)):
     for j in range(len(vendedor_atual)):
         venda = vendedor_atual[j]
         total += venda
-        if  venda > maior_individual["total"] :
+        if venda > maior_individual["total"]:
             maior_individual = {
-                "iDia":j,
-                "iVendedor":i,
+                "iDia": j,
+                "iVendedor": i,
                 "total": venda
 
             }
     total_vendas_vendedores.append(total)
 
 
-
 for i in range(len(vendas[0])):
     total = 0
     for j in range(len(vendas)):
-        total+= vendas[j][i] 
+        total += vendas[j][i]
     total_vendas_por_dia.append(total)
 
 
 maior = max(total_vendas_vendedores)
 maior_vendedor = {
     "index": total_vendas_vendedores.index(maior),
-    "total":maior
+    "total": maior
 }
 
 
-print( "Total de vendas: ",total_vendas_vendedores)
+print("Total de vendas: ", total_vendas_vendedores)
 
 print("O maior foi:", maior_vendedor)
 
@@ -53,8 +52,7 @@ print("Total por dia", total_vendas_por_dia)
 melhor = max(total_vendas_por_dia)
 melhor_dia = {
     "index": total_vendas_por_dia.index(melhor),
-    "total":melhor
-
+    "total": melhor
 }
 
 print("A maior venda individual foi:", maior_individual)
